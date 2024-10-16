097b4f687cb5d3d7b22d9bcf4eb81a54d4e6c669
Adeyemi <adediran.yemite@yahoo.com>
2024-10-16 21:17:26 +0100

    Update requirements file and setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "realEstate",
+    version="0.0.0",
+    author="Adeyemi",
+    author_email="adediran.yemite@yahoo.com",
+    packages=find_packages()
+)
