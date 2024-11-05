0614a50697a553478ca4c452dcbb635dd0ed2046
Adediran Adeyemi <65359919+Adeyemi0@users.noreply.github.com>
2024-11-05 08:25:48 +0100

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name = "realEstate",
-    version="0.0.0",
-    author="Adeyemi",
-    author_email="adediran.yemite@yahoo.com",
-    packages=find_packages()
-)
